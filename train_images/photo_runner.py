10783ecb7490b1faf8acece23081b4b57b67437d
Sourabh-Majumdar <msourabh970320@gmail.com>
2017-11-10 15:33:10 +0530

    updating

--- train_images/photo_runner.py
+++ train_images/photo_runner.py
@@ -1,10 +1,12 @@
 import os
 import glob
+import re
 os.chdir('.')
 
 for img_file in glob.glob("*.png") :
 	print(img_file)
-	img_file_components = img_file.split('_')
+	#img_file_components = img_file.split('_')
+	img_file_components = re.findall(r'\d+',img_file)
 	print(type(img_file))
 	print(img_file_components)
 	final_components = img_file_components[3:]
@@ -14,7 +16,10 @@ for img_file in glob.glob("*.png") :
 	print(" final components selected are ")
 	print(final_components)
 	print(type(final_components))
-	last_component = final_components[-1]
+	y_values = list(map(int,final_components))
+	print(" The y_values are ")
+	print(y_values)
+
 
 
 
